46a6276fe61bf9b4e3d0d2cd25d16624763b4d36
FudgyWudgy27 <Zer042711@gmail.com>
2025-04-01 16:15:20 -0400

    Created Express server with 2 pages and a 404 page

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+
+// Create an express app
+const app = express();
+
+app.use(
+    express.static(
+        path.join(__dirname, "views")
+    )
+);
+
+// app.get("/contact", function (req, res, next) {
+//      console.log(req.query);
+//      next();
+// }, function (req, res, next){
+//      res.send("<p>Limit</p>");
+// });
+
+app.get("/*page/", function(req, res){
+    res.sendFile(path.join(__dirname, "views/404.html"));
+});
+
+const port = process.env.PORT || 14913;
+
+app.listen(port);
+
+console.log(`Server running: http://localhost:${port}`);
